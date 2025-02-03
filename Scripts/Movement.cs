d8c8252376aad19675203741dc4f44b357364b41
Aleksi Malminen <aleksi.malminen@tuni.fi>
2025-02-03 13:36:54 +0200

    Move Foundation

--- Scripts/Movement.cs
+++ Scripts/Movement.cs
@@ -3,9 +3,9 @@ using System;
 
 public partial class Movement : Node2D
 {
-	[Export] int _speed = 10;
-	[Export] int _acceleration = 10;
-	[Export] int _slowdown = 10;
+	[Export] int _speed = 10; //Perus speed liikkumiselle
+	[Export] int _acceleration = 10; //Kiihtyvyys trukille koska ajoneuvot ei mene nollasta sataan samantien.
+	[Export] int _slowdown = 10; //Kuinka hitaasti trukki hidstaa kun kaasusta päästää irti.
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -14,5 +14,8 @@ public partial class Movement : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		Input.IsActionPressed("Break"); //A
+
+		Input.IsActionPressed("Accelerator"); //D
 	}
 }
