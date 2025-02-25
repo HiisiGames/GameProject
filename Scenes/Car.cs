31bc1f6025ebcd786006ad7a39e92defe5f87c0d
Juuso Porentola <juuso.poretola@tuni.fim>
2025-02-25 15:09:11 +0200

    Movement work

--- Scenes/Car.cs
+++ Scenes/Car.cs
@@ -1,22 +1,69 @@
 using Godot;
 using System;
-using System.Collections.Generic;
+using Godot.Collections;
 
 namespace TruckGame
 {
 	public partial class Car : RigidBody2D
 	{
+		[Export] private float _speed = 60000f;
+
+		[Export] private float _maxSpeed = 100f;
+
+
 		// Called when the node enters the scene tree for the first time.
-		private List<Wheel> WheelArray = new List<Wheel>();
+		private Array<Node> WheelArray = new Array<Node>();
 		public override void _Ready()
 		{
-			WheelArray = GetTree().GetNodesInGroup("Wheels");
-			
+			WheelArray = GetTree().GetNodesInGroup("wheel");
+
+			GD.Print("Wheel array count after _Ready: " + WheelArray.Count);  // Check count here
+
 		}
 
 		// Called every frame. 'delta' is the elapsed time since the previous frame.
 		public override void _Process(double delta)
 		{
+			_Movement((float)delta);
+		}
+
+		public void _Movement(float delta)
+		{
+			//A
+			if (Input.IsActionPressed("Break"))
+			{
+				GD.Print("break 1");
+				foreach (Node wheelNode in WheelArray)
+				{
+					GD.Print("break 2");
+					if (wheelNode is RigidBody2D wheel)
+					{
+						if (wheel.AngularVelocity > -_maxSpeed)
+						{
+							GD.Print("break toimii");
+							wheel.ApplyTorqueImpulse(-_speed * (float)delta);
+						}
+					}
+				}
+			}
+
+			//D
+			if (Input.IsActionPressed("Accelerate"))
+			{
+				foreach (Node wheelNode in WheelArray)
+				{
+					GD.Print("Accel 1 ");
+					if (wheelNode is RigidBody2D wheel)
+					{
+						GD.Print("Accel 2");
+						if (wheel.AngularVelocity < _maxSpeed)
+						{
+							GD.Print("Accel toimii");
+							wheel.ApplyTorqueImpulse(_speed * (float)delta);
+						}
+					}
+				}
+			}
 		}
 	}
 }
--- Scenes/Wheel.cs
+++ Scenes/Wheel.cs
@@ -1,19 +1,19 @@
 using Godot;
 using System;
 
+
 namespace TruckGame
 {
-		public partial class Wheel : RigidBody2D
+	public partial class Wheel : RigidBody2D
 	{
 		// Called when the node enters the scene tree for the first time.
-		public override void _Ready()
-		{
-		}
+		// public override void _Ready()
+		// {
+		// }
 
-		// Called every frame. 'delta' is the elapsed time since the previous frame.
-		public override void _Process(double delta)
-		{
-		}
+		// // Called every frame. 'delta' is the elapsed time since the previous frame.
+		// public override void _Process(double delta)
+		// {
+		// }
 	}
 }
-
