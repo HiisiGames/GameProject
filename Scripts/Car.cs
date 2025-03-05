574d50d445b535c3efc5194973bab7bfc61c79fd
Henri Hollanti <henri.hollanti@tuni.fi>
2025-03-05 13:55:45 +0200

    Artist created terrain textures added, changes to terrain shape, added left border collision shape for Level_2

--- Scripts/Car.cs
+++ Scripts/Car.cs
@@ -24,6 +24,9 @@ namespace TruckGame
 		// Called every frame. 'delta' is the elapsed time since the previous frame.
 		public override void _Process(double delta)
 		{
+		}
+		public override void _PhysicsProcess(double delta)
+		{
 			_Movement((float)delta);
 		}
 
