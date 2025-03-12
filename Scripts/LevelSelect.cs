b173aab1791b1d9426e6470812f15e0bd58e56e2
Juuso Porentola <juuso.poretola@tuni.fim>
2025-03-12 12:04:14 +0200

    Main menu

--- Scripts/LevelSelect.cs
+++ Scripts/LevelSelect.cs
@@ -7,13 +7,33 @@ namespace TruckGame
 	public partial class LevelSelect : Node
 	{
 		[Export] private string _mainMenuScenePath = "res://GUI/MainMenu.tscn";
+		[Export] private string _level1ScenePath = "res://Levels/level_1.tscn";
+		[Export] private string _level2ScenePath = "";
+		[Export] private string _level3ScenePath = "";
 		private Button _selectBack;
+		private Button _selectLevel1;
+		private Button _selectLevel2;
+		private Button _selectLevel3;
+
+
 		// Called when the node enters the scene tree for the first time.
 		public override void _Ready()
 		{
 			_selectBack = GetNode<Button>("BackButton");
 
 			_selectBack.Pressed += OnBackButtonPressed;
+
+			_selectLevel1 = GetNode<Button>("Level1");
+
+			_selectLevel1.Pressed += OnLevel1ButtonPressed;
+
+			_selectLevel2 = GetNode<Button>("Level2");
+
+			// _selectLevel2.Pressed += OnLevel2ButtonPressed;
+
+			_selectLevel3 = GetNode<Button>("Level3");
+
+			// _selectLevel3.Pressed += OnLevel3ButtonPressed;
 		}
 
 		// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,7 +43,7 @@ namespace TruckGame
 		private void OnBackButtonPressed()
 		{
 			PackedScene selectBackButton = ResourceLoader.Load<PackedScene>(_mainMenuScenePath);
-			if ( selectBackButton != null)
+			if (selectBackButton != null)
 			{
 				GetTree().ChangeSceneToPacked(selectBackButton);
 			}
@@ -31,7 +51,44 @@ namespace TruckGame
 			{
 				GD.Print("Main menu scene not found");
 			}
-
 		}
+		private void OnLevel1ButtonPressed()
+		{
+			PackedScene selectLevel1Button = ResourceLoader.Load<PackedScene>(_level1ScenePath);
+			if (selectLevel1Button != null)
+			{
+				GetTree().ChangeSceneToPacked(selectLevel1Button);
+			}
+			else
+			{
+				GD.Print("Level 1 scene not found");
+			}
+		}
+		// private void OnLevel2ButtonPressed()
+		// {
+		// 	PackedScene selectLevel2Button = ResourceLoader.Load<PackedScene>(_level2ScenePath);
+		// 	if ( selectLevel2Button != null)
+		// 	{
+		// 		GetTree().ChangeSceneToPacked(selectLevel2Button);
+		// 	}
+		// 	else
+		// 	{
+		// 		GD.Print("Level 2 scene not found");
+		// 	}
+
+		// }
+		// private void OnLevel3ButtonPressed()
+		// {
+		// 	PackedScene selectLevel3Button = ResourceLoader.Load<PackedScene>(_level3ScenePath);
+		// 	if ( selectLevel3Button != null)
+		// 	{
+		// 		GetTree().ChangeSceneToPacked(selectLevel3Button);
+		// 	}
+		// 	else
+		// 	{
+		// 		GD.Print("Level 3 scene not found");
+		// 	}
+
+		// }
 	}
 }
--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -7,16 +7,23 @@ namespace TruckGame
 	{
 		[Export] private string _levelScenePath = "res://GUI/LevelSelection.tscn";
 		[Export] private string _settingsScenePath = "res://GUI/Settings.tscn";
-		private Button _selectPlay;
-		private Button _selectSettings;
+		private TextureButton _selectPlay;
+		private TextureButton _selectSettings;
+		private TextureButton _selectCredits;
+		private PackedScene _selectSettingsScene;
+		private Panel _settingsPanel;
 		// Called when the node enters the scene tree for the first time.
 		public override void _Ready()
 		{
-			_selectPlay = GetNode<Button>("PlayButton");
-			_selectSettings = GetNode<Button>("SettingsButton");
+			_selectSettingsScene = ResourceLoader.Load<PackedScene>(_settingsScenePath);
+
+			_selectPlay = GetNode<TextureButton>("PlayButton");
+			_selectSettings = GetNode<TextureButton>("SettingsButton");
+			_selectCredits = GetNode<TextureButton>("CreditsButton");
 
 			_selectPlay.Pressed += OnPlayPressed;
 			_selectSettings.Pressed += OnSettingsPressed;
+			_selectCredits.Pressed += OnCreditsPressed;
 		}
 
 		// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,18 +36,31 @@ namespace TruckGame
 			if (selectLevelScene != null)
 			{
 				GetTree().ChangeSceneToPacked(selectLevelScene);
-			} else {
+			} else
+			{
 				GD.Print("Level selection scene not found");
 			}
 		}
 		private void OnSettingsPressed()
 		{
-			PackedScene selectSettingsScene = ResourceLoader.Load<PackedScene>(_settingsScenePath);
-			if (selectSettingsScene != null)
+
+			if (_selectSettingsScene != null)
 			{
-				GetTree().ChangeSceneToPacked(selectSettingsScene);
+				_settingsPanel = (Panel)_selectSettingsScene.Instantiate();
+				AddChild(_settingsPanel);
 			} else {
 				GD.Print("Settings scene not found");
+			}
+		}
+		private void OnCreditsPressed()
+		{
+			PackedScene selectLevelScene = ResourceLoader.Load<PackedScene>(_levelScenePath);
+			if (selectLevelScene != null)
+			{
+				GetTree().ChangeSceneToPacked(selectLevelScene);
+			} else
+			{
+				GD.Print("Level selection scene not found");
 			}
 		}
 	}
