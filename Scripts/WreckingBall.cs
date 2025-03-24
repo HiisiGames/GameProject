3560552dade3c5f61fff62081b0ff717ef55a4ac
Henri Hollanti <henri.hollanti@tuni.fi>
2025-03-24 09:35:22 +0200

    Modifications to WreckingBall.cs

--- Scripts/WreckingBall.cs
+++ Scripts/WreckingBall.cs
@@ -7,6 +7,9 @@ namespace TruckGame
     {
         private Vector2 _localForcePosition;
         private Vector2 _forceVector;
+        private Vector2 _directionVector;
+        [Export]
+        private float _force = 10.0f;
         public override void _Ready()
         {
             GD.Print($"Wrecking Ball global position: {this.GlobalPosition}");
@@ -14,11 +17,33 @@ namespace TruckGame
             _localForcePosition = ToLocal(GlobalForcePosition);
             GD.Print($"Ball global position: {GlobalForcePosition}");
             GD.Print($"Ball local position: {_localForcePosition}");
+            _directionVector = new Vector2(100, 0);
+            _forceVector = _directionVector * _force;
+            
+
+            //this.ApplyForce(_force, _localForcePosition);
+
         }
         public override void _PhysicsProcess(double delta)
         {
             base._PhysicsProcess(delta);
-            //this.ApplyForce();
+            //this.ApplyForce(_forceVector, _localForcePosition);
+            MoveWreckingBall((float)delta);
+        }
+        public void MoveWreckingBall(float delta)
+        {
+            if(_localForcePosition.X < 400)
+            {
+                _directionVector = new Vector2(-100, 0);
+                _forceVector = _directionVector * _force;
+                this.ApplyImpulse(_forceVector, _localForcePosition);
+            }
+            else
+            {
+                _directionVector = new Vector2(100, 0);
+                _forceVector = _directionVector * _force;
+                 this.ApplyImpulse(_forceVector , _localForcePosition);
+            }
         }
     }
 }
